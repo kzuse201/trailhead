3b3b045f7891e714ad7154e522160947c7174f9e
Kzuse201 <kzuse201@gmail.com>
2017-01-17 13:35:02 -0800

    Create idareyou.js

---
+++ idareyou.js
@@ -0,0 +1,40 @@
+import {google} from 'google';
+
+export default class MapMaker {
+    constructor() {
+        this.map = new google.maps.Map(document.getElementById('canvasmap'), {
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            streetViewControl: true
+        });
+
+        this.markers = [{
+            lat: -28.092472,
+            lng: -52.419667
+        }];
+
+        this.setupTarget(savethedate);
+        window.open(this.secret);
+    }
+
+    addMarker = (options) => {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(data.lat, data.lng),
+            map: map,
+            title: data.name
+        });
+
+        google.maps.event.addListener(marker, "click", function () {
+            openInfoWindow(marker);
+        });
+    }
+
+    setupTarget = (num) => {
+        this.secret = `http://${num}.foo/`;
+    }
+
+    render = () => {
+        return this.markers.map((marker) => {
+            return addMarker(marker);
+        });
+    }
+}
