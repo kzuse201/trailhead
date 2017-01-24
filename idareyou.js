550bb0c6bc76da2e79b56aebbea6f29ac236398e
zetlen <zetlen@gmail.com>
2017-01-24 16:50:47 -0600

    fix(style): use jlongster/prettier to format js in 2017

--- idareyou.js
+++ idareyou.js
@@ -1,40 +1,37 @@
-import {google} from 'google';
+import { google } from "google";
 
 export default class MapMaker {
-    constructor() {
-        this.map = new google.maps.Map(document.getElementById('canvasmap'), {
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            streetViewControl: true
-        });
+  constructor() {
+    this.map = new google.maps.Map(document.getElementById("canvasmap"), {
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      streetViewControl: true
+    });
 
-        this.markers = [{
-            lat: -28.092472,
-            lng: -52.419667
-        }];
+    this.markers = [ { lat: -28.092472, lng: -52.419667 } ];
 
-        this.setupTarget(savethedate);
-        window.open(this.secret);
-    }
+    this.setupTarget(savethedate);
+    window.open(this.secret);
+  }
 
-    addMarker = (options) => {
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(data.lat, data.lng),
-            map: map,
-            title: data.name
-        });
+  addMarker = data => {
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(data.lat, data.lng),
+      map: map,
+      title: data.name
+    });
 
-        google.maps.event.addListener(marker, "click", function () {
-            openInfoWindow(marker);
-        });
-    }
+    google.maps.event.addListener(marker, "click", function() {
+      openInfoWindow(marker);
+    });
+  };
 
-    setupTarget = (num) => {
-        this.secret = `http://${num}.foo/`;
-    }
+  setupTarget = num => {
+    this.secret = `http://${num}.foo/`;
+  };
 
-    render = () => {
-        return this.markers.map((marker) => {
-            return addMarker(marker);
-        });
-    }
+  render = () => {
+    return this.markers.map(marker => {
+      return addMarker(marker);
+    });
+  };
 }
